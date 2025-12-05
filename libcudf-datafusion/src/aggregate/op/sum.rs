9553390e4f19cbd1b13b0054401e1868e1a77c76
Filippo Rossi <git@filippo.dev>
2025-12-05 14:03:15 +0000

    chore: utility function

--- libcudf-datafusion/src/aggregate/op/sum.rs
+++ libcudf-datafusion/src/aggregate/op/sum.rs
@@ -4,8 +4,18 @@ use datafusion::common::exec_err;
 use datafusion::error::Result;
 use libcudf_rs::{AggregationOp, AggregationRequest, CuDFColumnView};
 use std::fmt::Debug;
+use std::sync::Arc;
+use datafusion::functions_aggregate::sum::{sum_udaf, Sum};
+use datafusion_expr::AggregateUDF;
+use crate::aggregate::op::udf::CuDFAggregateUDF;
 
-#[derive(Debug)]
+pub fn sum() -> Arc<AggregateUDF> {
+    let udf = CuDFAggregateUDF::new(Arc::new(Sum::default()), Arc::new(CuDFSum));
+    Arc::new(AggregateUDF::new_from_impl(udf))
+}
+
+
+#[derive(Debug, Default)]
 pub struct CuDFSum;
 
 impl CuDFAggregationOp for CuDFSum {
