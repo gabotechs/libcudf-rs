cc06bf4ebf9f9c4d962064b8f77a1dcb7475506f
gabotechs <gb.mt.me@gmail.com>
2025-12-07 17:29:18 +0000

    Add data_type compatibility layer

--- libcudf-datafusion/src/physical/cudf_load.rs
+++ libcudf-datafusion/src/physical/cudf_load.rs
@@ -1,5 +1,6 @@
 use crate::errors::cudf_to_df;
 use arrow::array::{Array, RecordBatch};
+use arrow_schema::{ArrowError, DataType, Field, FieldRef, Schema, SchemaRef};
 use datafusion::common::{exec_err, plan_err};
 use datafusion::execution::{SendableRecordBatchStream, TaskContext};
 use datafusion_physical_plan::stream::RecordBatchStreamAdapter;
@@ -64,10 +65,12 @@ impl ExecutionPlan for CuDFLoadExec {
         context: Arc<TaskContext>,
     ) -> datafusion::common::Result<SendableRecordBatchStream> {
         let host_stream = self.input.execute(partition, context)?;
-        let schema = host_stream.schema();
-        let cudf_stream = host_stream.map(|batch_or_err| {
+        let schema = cudf_schema_compatibility_map(host_stream.schema());
+        let target_schema = Arc::clone(&schema);
+
+        let cudf_stream = host_stream.map(move |batch_or_err| {
             let batch = match batch_or_err {
-                Ok(batch) => batch,
+                Ok(batch) => cudf_batch_compatibility_map(batch, Arc::clone(&target_schema))?,
                 Err(err) => return Err(err),
             };
 
@@ -87,4 +90,49 @@ impl ExecutionPlan for CuDFLoadExec {
         });
         Ok(Box::pin(RecordBatchStreamAdapter::new(schema, cudf_stream)))
     }
+}
+
+fn cudf_schema_compatibility_map(schema: SchemaRef) -> SchemaRef {
+    let mut new_fields = Vec::with_capacity(schema.fields.len());
+
+    for field in schema.fields() {
+        if field.data_type() == &DataType::Utf8View {
+            new_fields.push(FieldRef::new(Field::new(
+                field.name(),
+                DataType::Utf8,
+                field.is_nullable(),
+            )));
+        } else {
+            new_fields.push(Arc::clone(field));
+        }
+    }
+
+    SchemaRef::new(Schema::new(new_fields))
+}
+
+fn cudf_batch_compatibility_map(
+    batch: RecordBatch,
+    target_schema: SchemaRef,
+) -> Result<RecordBatch, ArrowError> {
+    let mut new_cols = Vec::with_capacity(batch.num_columns());
+
+    let original_schema = batch.schema();
+    let original_fields = original_schema.fields();
+    let target_fields = target_schema.fields();
+
+    for ((col, original_field), target_field) in batch
+        .columns()
+        .iter()
+        .zip(original_fields)
+        .zip(target_fields)
+    {
+        // CuDF does not support UTF8View, so we cast it to UTF8.
+        if original_field.data_type() != target_field.data_type() {
+            new_cols.push(arrow::compute::cast(&col, target_field.data_type())?);
+        } else {
+            new_cols.push(Arc::clone(col));
+        }
+    }
+
+    RecordBatch::try_new(target_schema, new_cols)
 }
