35159606d036d1417576afdae9a312bf23dc3519
gabotechs <gb.mt.me@gmail.com>
2025-12-11 14:14:43 +0000

    Do not delegate to child ProjectionExec

--- libcudf-datafusion/src/physical/projection.rs
+++ libcudf-datafusion/src/physical/projection.rs
@@ -1,42 +1,86 @@
 use crate::expr::expr_to_cudf_expr;
-use datafusion::common::Statistics;
+use arrow::array::RecordBatch;
+use arrow::datatypes::SchemaRef;
+use arrow::record_batch::RecordBatchOptions;
 use datafusion::config::ConfigOptions;
 use datafusion::error::DataFusionError;
-use datafusion::execution::{SendableRecordBatchStream, TaskContext};
+use datafusion::execution::context::DataFilePaths;
+use datafusion::execution::{RecordBatchStream, SendableRecordBatchStream, TaskContext};
+use datafusion::physical_expr::equivalence::ProjectionMapping;
+use datafusion::physical_expr::projection::ProjectionExprs;
 use datafusion_physical_plan::execution_plan::CardinalityEffect;
 use datafusion_physical_plan::filter_pushdown::{FilterDescription, FilterPushdownPhase};
-use datafusion_physical_plan::metrics::MetricsSet;
+use datafusion_physical_plan::metrics::{BaselineMetrics, ExecutionPlanMetricsSet, MetricsSet};
 use datafusion_physical_plan::projection::{ProjectionExec, ProjectionExpr};
 use datafusion_physical_plan::{
-    DisplayAs, DisplayFormatType, ExecutionPlan, PhysicalExpr, PlanProperties,
+    DisplayAs, DisplayFormatType, ExecutionPlan, ExecutionPlanProperties, PhysicalExpr,
+    PlanProperties,
 };
 use delegate::delegate;
+use futures::stream::{Stream, StreamExt};
 use std::any::Any;
 use std::fmt::Formatter;
+use std::pin::Pin;
 use std::sync::Arc;
+use std::task::{Context, Poll};
 
 #[derive(Debug)]
 pub struct CuDFProjectionExec {
     host_exec: ProjectionExec,
-    cudf_exec: ProjectionExec,
+    cudf_exprs: ProjectionExprs,
+    properties: PlanProperties,
+    metrics: ExecutionPlanMetricsSet,
 }
 
 impl CuDFProjectionExec {
     pub fn try_new(host_exec: ProjectionExec) -> Result<Self, DataFusionError> {
-        let original_expressions = host_exec.expr();
-        let mut expressions = Vec::with_capacity(original_expressions.len());
-        for expr in original_expressions {
-            expressions.push(ProjectionExpr {
-                alias: expr.alias.to_string(),
-                expr: expr_to_cudf_expr(expr.expr.as_ref())?,
+        let cudf_exprs = host_exec
+            .expr()
+            .iter()
+            .map(|v| {
+                Ok::<_, DataFusionError>(ProjectionExpr {
+                    alias: v.alias.clone(),
+                    expr: expr_to_cudf_expr(v.expr.as_ref())?,
+                })
             })
-        }
+            .collect::<Result<Vec<_>, _>>()?;
 
-        let cudf_exec = ProjectionExec::try_new(expressions, Arc::clone(host_exec.input()))?;
+        let projection_exprs = ProjectionExprs::new(cudf_exprs);
+        let input_schema = host_exec.input().schema();
+        let output_schema = Arc::new(projection_exprs.project_schema(&input_schema)?);
+
+        // Construct a map from the input expressions to the output expression of the Projection
+        let projection_mapping = projection_exprs.projection_mapping(&input_schema)?;
+        let properties =
+            Self::compute_properties(host_exec.input(), &projection_mapping, output_schema)?;
+
         Ok(Self {
             host_exec,
-            cudf_exec,
+            cudf_exprs: projection_exprs,
+            properties,
+            metrics: ExecutionPlanMetricsSet::new(),
         })
+    }
+
+    fn compute_properties(
+        input: &Arc<dyn ExecutionPlan>,
+        projection_mapping: &ProjectionMapping,
+        schema: SchemaRef,
+    ) -> Result<PlanProperties, DataFusionError> {
+        // Calculate equivalence properties:
+        let input_eq_properties = input.equivalence_properties();
+        let eq_properties = input_eq_properties.project(projection_mapping, schema);
+        // Calculate output partitioning, which needs to respect aliases:
+        let output_partitioning = input
+            .output_partitioning()
+            .project(projection_mapping, input_eq_properties);
+
+        Ok(PlanProperties::new(
+            eq_properties,
+            output_partitioning,
+            input.pipeline_behavior(),
+            input.boundedness(),
+        ))
     }
 }
 
@@ -72,7 +116,19 @@ impl ExecutionPlan for CuDFProjectionExec {
         partition: usize,
         context: Arc<TaskContext>,
     ) -> datafusion::common::Result<SendableRecordBatchStream> {
-        self.cudf_exec.execute(partition, context)
+        let input = self.host_exec.input().execute(partition, context)?;
+        let baseline_metrics = BaselineMetrics::new(&self.metrics, partition);
+
+        Ok(Box::pin(CuDFProjectionStream {
+            schema: self.schema(),
+            expr: self.cudf_exprs.expr_iter().collect(),
+            input,
+            baseline_metrics,
+        }))
+    }
+
+    fn properties(&self) -> &PlanProperties {
+        &self.properties
     }
 
     delegate! {
@@ -86,12 +142,66 @@ impl ExecutionPlan for CuDFProjectionExec {
         }
     }
 
-    delegate! {
-        to self.cudf_exec {
-            fn properties(&self) -> &PlanProperties;
-            fn metrics(&self) -> Option<MetricsSet>;
-            fn partition_statistics(&self, partition: Option<usize>) -> datafusion::common::Result<Statistics>;
+    fn metrics(&self) -> Option<MetricsSet> {
+        Some(self.metrics.clone_inner())
+    }
+}
+
+struct CuDFProjectionStream {
+    schema: SchemaRef,
+    expr: Vec<Arc<dyn PhysicalExpr>>,
+    input: SendableRecordBatchStream,
+    baseline_metrics: BaselineMetrics,
+}
+
+impl CuDFProjectionStream {
+    fn batch_project(&self, batch: &RecordBatch) -> Result<RecordBatch, DataFusionError> {
+        // Records time on drop
+        let _timer = self.baseline_metrics.elapsed_compute().timer();
+        let arrays = self
+            .expr
+            .iter()
+            .map(|expr| {
+                expr.evaluate(batch)
+                    .and_then(|v| v.into_array(batch.num_rows()))
+            })
+            .collect::<Result<Vec<_>, _>>()?;
+
+        if arrays.is_empty() {
+            let options = RecordBatchOptions::new().with_row_count(Some(batch.num_rows()));
+            RecordBatch::try_new_with_options(Arc::clone(&self.schema), arrays, &options)
+        } else {
+            RecordBatch::try_new(Arc::clone(&self.schema), arrays)
         }
+        .map_err(|err| {
+            DataFusionError::ArrowError(
+                Box::new(err),
+                Some("Error projecting CuDF RecordBatch".to_string()),
+            )
+        })
+    }
+}
+
+impl Stream for CuDFProjectionStream {
+    type Item = Result<RecordBatch, DataFusionError>;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let poll = self.input.poll_next_unpin(cx).map(|x| match x {
+            Some(Ok(batch)) => Some(self.batch_project(&batch)),
+            other => other,
+        });
+
+        self.baseline_metrics.record_poll(poll)
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.input.size_hint()
+    }
+}
+
+impl RecordBatchStream for CuDFProjectionStream {
+    fn schema(&self) -> SchemaRef {
+        Arc::clone(&self.schema)
     }
 }
 
